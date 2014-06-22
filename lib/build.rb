abb810a9536928a9263c1d7dda102966521451d1
Yaro Apletov <yaro@mail.ru>
2014-06-22 23:43:30 +0100

    Added conditional for only one sendUrl

--- lib/build.rb
+++ lib/build.rb
@@ -2,7 +2,7 @@ require 'debugger'
 xml_template = File.read('./lib/template.xml')
 html = File.read('./lib/websiteone.html')
 js = File.read('./lib/HangoutConnection.js')
-output = File.open('./lib/websiteone.xml', 'w')
+output = File.open('./public/websiteone.xml', 'w')
 
 html.gsub!('$JS_PLACEHOLDER',js.to_s)
 xml_template.gsub!('$HTML_PLACEHOLDER', html.to_s)
